dfd437f567f05f711822d2a2f4c82ad027a00d4b
Jeff Brannon <jeff@piczelspydr.co.uk>
2019-05-17 17:12:44 +0100

    audit API Endpoints

--- main.go
+++ main.go
@@ -4,40 +4,36 @@ import (
 	"fmt"
 	"github.com/gorilla/mux"
 	"github.com/hastechnologyltd/ordis-core/audit"
+	"log"
 	"net/http"
 )
 
+var audits *audit.Audits
+
 func main() {
 
-	audits := audit.CreateAudit()
+	audits = audit.CreateAudit()
 
-	audits.AddAudit("Audit One")
-	audits.AddAudit("Audit Two")
-	audits.AddAudit("Audit Three")
+	router := mux.NewRouter().StrictSlash(true)
+	router.HandleFunc("/", Index)
+	//router.HandleFunc("/audit", AddAudit)
+	router.HandleFunc("/audit/{auditData}", AddAudit)
 
-	//var size = unsafe.Sizeof(audits)
-	//fmt.Println(size)
-
-	audits.Display()
-
-	//router := mux.NewRouter().StrictSlash(true)
-	//router.HandleFunc("/", Index)
-	//router.HandleFunc("/todos", TodoIndex)
-	//router.HandleFunc("/todos/{todoId}", TodoShow)
-	//
-	//log.Fatal(http.ListenAndServe(":8080", router))
+	log.Fatal(http.ListenAndServe(":8080", router))
 }
 
 func Index(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprintln(w, "Welcome!")
 }
 
-func TodoIndex(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintln(w, "Todo Index!")
-}
+//func AddAudit(w http.ResponseWriter, r *http.Request) {
+//	fmt.Fprintln(w, "Todo Index!")
+//}
 
-func TodoShow(w http.ResponseWriter, r *http.Request) {
+func AddAudit(w http.ResponseWriter, r *http.Request) {
 	vars := mux.Vars(r)
-	todoId := vars["todoId"]
-	fmt.Fprintln(w, "Todo show:", todoId)
+	auditData := vars["auditData"]
+	audits.AddAudit(auditData)
+	//fmt.Fprintln(w, "Todo show:", auditData)
+	audits.Display()
 }
